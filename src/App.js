8ea82d8c89a572c4404a79bd99ce3f0ee4a96900
dilina-c <2019s17383@stu.cmb.ac.lk>
2023-05-21 04:46:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import katie_zaferes from "./images/katie-zaferes.png";
-
+import data from './data';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const cardData = data.map((item) => {
+    return < Card
+      key={item.id}
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      title={item.title}
+      price={item.price}
+      country={item.location}
+      id={item.id}
+      openspots={item.openSpots} />
+  })
   return (
     <div>
       <Navbar />
       {/* <Hero /> */}
-      <Card 
-        img={katie_zaferes}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className='card-list'>{cardData}</section>
+      
+
     </div>
   );
 }
