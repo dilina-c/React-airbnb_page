fa4d8187588d8ecbd49fd696c56dafa509799329
dilina-c <2019s17383@stu.cmb.ac.lk>
2023-05-22 15:56:50 +0530

    fourth commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,22 +3,22 @@ import star from "../images/Star.png";
 
 export default function Card(props) {
     let badgetext
-    if (props.item.openspots===0 )
+    if (props.openspots===0 )
         {badgetext="SOLDOUT"}
-    else if (props.item.location==="Online")
+    else if (props.location==="Online")
         {badgetext="ONLINE"}
     return(
         <div className="card">
             {badgetext && <div className="card-badge">{badgetext}</div>}
-            <img src={props.item.coverImg} alt={props.item.id} className="carditem"></img>
+            <img src={props.coverImg} alt={props.id} className="carditem"></img>
             <div className="card-stats">
             <img src={star} alt="card rating" className="star-rating"></img>
-            <span>{props.item.stats.rating}</span>
-            <span className="gray">({props.item.stats.reviewCount})</span>
-            <span className="gray"> {props.item.location} </span>
+            <span>{props.stats.rating}</span>
+            <span className="gray">({props.stats.reviewCount})</span>
+            <span className="gray"> {props.location} </span>
             </div>
-            <p>{props.item.title}</p>
-            <p><span className="bold">From ${props.item.price}</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,11 +3,11 @@ import Picturgrp from "../images/PIctureGroup.png";
 
 export default function Hero() {
     return(
-        <div>
-            <img src={Picturgrp} alt="picture-group" className="pic-grp"/>
-            <h1 className="header-hero">Online Experiences</h1>
-            <p className="paragraph-hero">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+        <section className="hero">            
+        <img src={Picturgrp} alt="picture-group" className="pic-grp"/>
+        <h1 className="header-hero">Online Experiences</h1>
+        <p className="paragraph-hero">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
-        </div>
+        </section>
     )
 }
